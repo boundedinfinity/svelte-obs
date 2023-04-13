cc358f212e85f3b67c4e7f934ed4c3d20c6b3e2d
bbabb <brad.babb@boundedinfinity.com>
2023-04-12 22:57:22 -0500

    updates

--- bin/voice-dictation.fish
+++ bin/voice-dictation.fish
@@ -3,4 +3,4 @@
 set script_dir (cd (dirname (status -f)); and pwd)
 source $script_dir/utils.fish
 
-chrome 'https://docs.google.com/document/d/1v_bogUE3DdIskjOu2hQ2v5gKVJPT22lrTG1guzb8vcg/edit?usp=share_link'
+chrome ''
